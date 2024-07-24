c6a2fea8be68e651179da91f5fbaa45fc008ce30
Emanuel Patrick <emanuelpatrick440@gmail.com>
2024-07-24 17:22:23 -0300

    check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -19,6 +19,10 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
 
+    public ChessPosition getChessPosition() {
+        return ChessPosition.fromPosition(position);
+    }
+
     // Diferencia as peças. Adversária ou do Oponente
     protected boolean isThereOpponentPiece(Position position) {
         ChessPiece p = (ChessPiece)getBoard().piece(position);
